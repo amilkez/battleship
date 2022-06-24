115b2b663483045229987ac72aa41bff6420372f
angel <angelhidalgomb@gmail.com>
2022-06-24 04:33:18 -0500

    Create ship factory function

---
+++ src/modules/ship.js
@@ -0,0 +1,18 @@
+const shipFactory = (length) => {
+  const hitsPositions = [];
+
+  const hit = (position) => {
+    hitsPositions.push(position);
+  };
+
+  const isSunk = () => {
+    if (hitsPositions.length === length) {
+      return true;
+    }
+    return false;
+  };
+
+  return { hit, isSunk, hitsPositions };
+};
+
+export { shipFactory };
